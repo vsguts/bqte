5c69c7975f467eaa3311adec5a89bb4b0d24c13e
vsguts <vsguts@gmail.com>
2018-12-17 00:33:51 +0200

    Babel and Webpack were added.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,29 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-    context: __dirname,
-    mode: "development",
-    devtool: "source-map",
-    entry: "./index.js",
-    output: {
-        path: __dirname + "/dist",
-        filename: "bundle.js"
-    }
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
 };
